296d1939d68d41df6affcea416113c4be4f69357
seanking8 <g00367593@gmit.ie>
2020-01-20 17:28:54 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -4,7 +4,8 @@ class HelloWorld {
 
     public static void main(String[] args) {
         // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
     }
-
 }
